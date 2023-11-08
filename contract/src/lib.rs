092dd7f2ae671edaa9a89fdafa111f89eb081b1c
lennart Czardybon <lennczar@gmail.com>
2023-11-08 16:24:20 +0100

    function heads for verifier contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,23 +1,49 @@
 use near_sdk::{
     borsh::{self, BorshSerialize, BorshDeserialize},
-    near_bindgen, env,
+    near_bindgen, env, AccountId, collections::{LookupSet, LookupMap},
 };
 use std::collections::HashMap;
 
+type CredentialSchema = String; 
+type CredentialHash = String;
+
+type AcIssuer = AccountId;
+type AcHolder = AccountId;
+type AcRP = AccountId;
+
+
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct StatusMessage {
-    records: HashMap<String, String>,
+pub struct Contract {
+    credentialSchemata: LookupMap<AcIssuer, LookupSet<CredentialSchema>>,
+    credentialHashes: LookupMap<(AcIssuer, CredentialSchema), LookupSet<CredentialHashes>>
 }
 
+
 #[near_bindgen]
-impl StatusMessage {
-    pub fn set_status(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-        self.records.insert(account_id.to_string(), message);
+impl Contract {
+    
+    // TODO decide whether functions should be payable
+
+    pub fn addCredentialSchema(&mut self, credentialSchema: CredentialSchema) {
+        self.credentialSchemata.insert(env::predecessor_account_id(), credentialSchema);
     }
 
-    pub fn get_status(&self, account_id: String) -> Option<String> {
-        self.records.get(&account_id).cloned()
+    pub fn modifyCredentialHashes(
+        &mut self, 
+        schemaId: CredentialSchemaId,
+        add: Vec<CredentialHash>,
+        remove: Vec<CredentialHash>
+    ) {
+
     }
+
+    // pub fn set_status(&mut self, message: String) {
+    //     let account_id = env::signer_account_id();
+    //     self.records.insert(account_id.to_string(), message);
+    // }
+
+    // pub fn get_status(&self, account_id: String) -> Option<String> {
+    //     self.records.get(&account_id).cloned()
+    // }
 }
