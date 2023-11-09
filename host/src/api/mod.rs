b855cde171f6fdbed1eb14e9c4095aa6285ca691
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2023-11-09 03:54:22 +0000

    Fix CORS issue on backend API

--- host/src/api/mod.rs
+++ host/src/api/mod.rs
@@ -1,17 +1,24 @@
 mod routers;
 
-use axum::Router;
+use axum::{Router, http::Method};
 use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 use routers::{
     hello::hello_router,
     holder::holder_router,
 };
+use tower_http::cors::{CorsLayer, Any};
 
 
 pub async fn api_start() {
+    let cors = CorsLayer::new()
+        .allow_methods(vec![Method::POST])
+        .allow_origin(Any)
+        .allow_headers(Any);
+
     let api_routes = Router::new()
         .nest("/hello", hello_router())
-        .nest("/holder", holder_router());
+        .nest("/holder", holder_router())
+        .layer(cors);
 
     let addr = SocketAddr::from((
         IpAddr::V4(Ipv4Addr::LOCALHOST),
